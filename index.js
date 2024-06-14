6f88d5ab7a13402f561754ed44fadf0701f0e0ee
Felix Kirchhoff <f.kirchhoff@proton.me>
2024-06-14 10:03:32 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,50 @@
+console.clear();
+
+const text = "hello ducks!"; // string
+let count = 5; // number
+const isOldEnough = true; // boolean true / false
+const notThere = null; // nothing
+const notevenSpecified = undefined; // default value in JS
+
+// console.log(text);
+// console.log("text");
+
+// console.log(count);
+
+count = 8;
+
+// console.log(count);
+
+const firstNumber = 4;
+const secondNumber = 2;
+
+console.log(firstNumber + secondNumber);
+console.log(firstNumber - secondNumber);
+console.log(firstNumber * secondNumber);
+console.log(firstNumber / secondNumber);
+
+console.log("---");
+
+const maxTime = 12;
+let currentTime = 7;
+
+console.log(currentTime % maxTime); // modulo / remainder
+// currentTime = currentTime + 1;
+// currentTime += 1;
+currentTime++;
+console.log(currentTime % maxTime);
+
+currentTime++;
+console.log(currentTime % maxTime);
+
+currentTime++;
+console.log(currentTime % maxTime);
+
+currentTime++;
+console.log(currentTime % maxTime);
+
+currentTime++;
+console.log(currentTime % maxTime);
+
+currentTime++;
+console.log(currentTime % maxTime);
